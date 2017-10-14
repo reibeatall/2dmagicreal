7fdf949eac1c92c43af87323da78a73a9002ade6
reibeatall <reibeatall@gmail.com>
2017-10-13 20:25:23 -0700

    Initial
    fir

---
+++ Assets/2DMagicGame/Scripts/cardRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardRotate : MonoBehaviour {
+    public GameObject gameObject;
+    public int rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(Vector3.up * rotateSpeed);
+	}
+}
