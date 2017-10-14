232533ba3074d2401e0e47dea9460c356f2699c3
reibeatall <reibeatall@gmail.com>
2017-10-13 21:33:27 -0700

    added card and spinning and level load
    I did a thing!

--- Assets/2DMagicGame/Scripts/cardRotate.cs
+++ Assets/2DMagicGame/Scripts/cardRotate.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class cardRotate : MonoBehaviour {
     public GameObject gameObject;
     public int rotateSpeed;
+    public string levelName;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+   }
+
+    void OnMouseOver()
+    {
         gameObject.transform.Rotate(Vector3.up * rotateSpeed);
-	}
+        if (Input.GetMouseButton(0)){
+
+            Application.LoadLevel(levelName);
+        }
+    }
 }
